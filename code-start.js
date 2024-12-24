971e5aa83aab312c597141e0bac972c7c9e88ab2
Vasiliy Kurganov <wkurganow@gmail.com>
2024-12-24 15:40:02 +0700

    Добавил еще одно решение для задачи номер 2

--- code-start.js
+++ code-start.js
@@ -1,9 +1,7 @@
-// 1. Сколько раз каждый элемент встречается в массиве:
-// {kiwi: 3, apple: 2, orange: 1}
-
 // 2. Создать массив который содержит только уникальные значения
 // ['kiwi', 'apple', 'orange']
 
+//Решение один
 const myFruits = ["kiwi", "apple", "kiwi", "orange", "kiwi", "apple"];
 function getUniqueFruits(list) {
   const unique = {};
@@ -14,34 +12,9 @@ function getUniqueFruits(list) {
   });
   return Object.keys(unique);
 }
-console.log(getUniqueFruits(myFruits));
+// console.log(getUniqueFruits(myFruits));
 
-// 3. Создать функцию, которая сгруппирует студентов по возрасту.
-// На выходе получить объект, где ключ - возраст,
-// а значение - массив студентов
-// {
-//   '20': [{ name: 'alex', age: 20 }, { name: 'masha', age: 20 }],
-//   '24': [{ name: 'mike', age: 24 }],
-//   '18': [{ name: 'stas', age: 18 }],
-// }
-const students = [
-  { name: "alex", age: 20 },
-  { name: "mike", age: 24 },
-  { name: "masha", age: 20 },
-  { name: "stas", age: 18 },
-];
-
-// 5. Получить единый массив из любимых пицц каждого друга
-// ['cheese', 'pepperoni', 'meat', 'fish']
-const friends = [
-  { name: "alex", pizzas: ["cheese", "pepperoni"] },
-  { name: "mike", pizzas: ["salami", "margarita"] },
-  { name: "stas", pizzas: ["meat"] },
-  { name: "anna", pizzas: ["fish"] },
-];
-
-// 6. Записать строку (символы строки) в обратном порядке (2 способа)
-// pizza => azzip
-const myStr = "pizza";
-
-const anyArray = [60, 20, 100, 1, 2, 3];
+//Решение два
+const setFruits = new Set(myFruits);
+const arrayWithFruits = Array.from(setFruits);
+console.log(arrayWithFruits);
