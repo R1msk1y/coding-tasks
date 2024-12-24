61a4a9b3655a34601599b4f86f8ea5690a1063fd
Vasiliy Kurganov <wkurganow@gmail.com>
2024-12-24 23:06:48 +0700

    Первый вариант решения задачи на палиндром, развернутый вариант

--- code-start.js
+++ code-start.js
@@ -1,37 +1,25 @@
-// 1. Сколько раз каждый элемент встречается в массиве:
-// {kiwi: 3, apple: 2, orange: 1}
+//Задача на палиндром - нужно написать функцию, которая возвращает true если переданная строка читается с права налево и с лева на право - одинаково и false если нет.
 
-// 2. Создать массив который содержит только уникальные значения
-// ['kiwi', 'apple', 'orange']
+//Палиндром это строка которую можно перевернуть
+const palindrom = (str)=>{
+    //Переводим строку в нижний регистр
+    str = str.toLowerCase()
+    //Разбиваем строку на символы, через
+    // занесение в массив
+    let str2 = str.split('');
+    //разворачиваем массив
+    str2 = str2.reverse();
+    //объеденить в строку
+    str2 = str2.join('');
+    if(str == str2){
+        return true
+    } else{
+        return false
+    }
 
-const myFruits = ["kiwi", "apple", "kiwi", "orange", "kiwi", "apple"];
 
-// 3. Создать функцию, которая сгруппирует студентов по возрасту.
-// На выходе получить объект, где ключ - возраст,
-// а значение - массив студентов
-// {
-//   '20': [{ name: 'alex', age: 20 }, { name: 'masha', age: 20 }],
-//   '24': [{ name: 'mike', age: 24 }],
-//   '18': [{ name: 'stas', age: 18 }],
-// }
-const students = [
-  { name: "alex", age: 20 },
-  { name: "mike", age: 24 },
-  { name: "masha", age: 20 },
-  { name: "stas", age: 18 },
-];
+    return str2;
 
-// 5. Получить единый массив из любимых пицц каждого друга
-// ['cheese', 'pepperoni', 'meat', 'fish']
-const friends = [
-  { name: "alex", pizzas: ["cheese", "pepperoni"] },
-  { name: "mike", pizzas: ["salami", "margarita"] },
-  { name: "stas", pizzas: ["meat"] },
-  { name: "anna", pizzas: ["fish"] },
-];
-
-// 6. Записать строку (символы строки) в обратном порядке (2 способа)
-// pizza => azzip
-const myStr = "pizza";
-
-const anyArray = [60, 20, 100, 1, 2, 3];
+}
+console.log(palindrom('abba'));
+console.log(palindrom('hello'));
